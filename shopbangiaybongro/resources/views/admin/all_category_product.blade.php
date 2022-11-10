63d2ebf99282281d9ee478b95fed20d22636d3e1
GrizzlyWin <94659314+GrizzlyWin@users.noreply.github.com>
2022-11-10 10:00:37 +0700

    1

---
+++ shopbangiaybongro/resources/views/admin/all_category_product.blade.php
@@ -0,0 +1,87 @@
+
+@extends('admin_layout')
+@section('admin_content')
+<div class="table-agile-info">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      Mô tả danh mục sản phẩm
+    </div>
+    <div class="row w3-res-tb">
+      <div class="col-sm-5 m-b-xs">
+        <select class="input-sm form-control w-sm inline v-middle">
+          <option value="0">Chọn hàng loạt</option>
+          <option value="1">Xóa lựa chọn</option>
+          <option value="2">Chỉnh sửa hàng loạt</option>
+          <option value="3">Export</option>
+        </select>
+        <button class="btn btn-sm btn-default">Thực hiện</button>                
+      </div>
+      <div class="col-sm-4">
+      </div>
+      <div class="col-sm-3">
+        <div class="input-group">
+          <input type="text" class="input-sm form-control" placeholder="Search">
+          <span class="input-group-btn">
+            <button class="btn btn-sm btn-default" type="button">Go!</button>
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="table-responsive">
+      <table class="table table-striped b-t b-light">
+        <thead>
+          <tr>
+            <th style="width:20px;">
+              <label class="i-checks m-b-none">
+                <input type="checkbox"><i></i>
+              </label>
+            </th>
+            <th>Tên danh mục</th>
+            <th>Hiển thị</th>
+            
+            <th style="width:30px;"></th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($all_category_product as $key => $cate_pro)
+          <tr>
+            <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td>
+            <td>{{$cate_pro->category_name}}</td>
+            <td><span class="text-ellipsis">
+             <?php
+             if($cate_pro->category_status==0){
+              echo 'Ẩn';
+             }else{
+              echo 'Hiện';
+             }
+              ?>
+              </span></td>
+            <td>
+              <a href="" class="active" ui-toggle-class=""><i class="fa fa-check text-success text-active"></i><i class="fa fa-times text-danger text"></i></a>
+            </td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+    <footer class="panel-footer">
+      <div class="row">
+        
+        <div class="col-sm-5 text-center">
+          <small class="text-muted inline m-t-sm m-b-sm">Hiển thị 20-30 trong số 50 danh mục hàng</small>
+        </div>
+        <div class="col-sm-7 text-right text-center-xs">                
+          <ul class="pagination pagination-sm m-t-none m-b-none">
+            <li><a href=""><i class="fa fa-chevron-left"></i></a></li>
+            <li><a href="">1</a></li>
+            <li><a href="">2</a></li>
+            <li><a href="">3</a></li>
+            <li><a href="">4</a></li>
+            <li><a href=""><i class="fa fa-chevron-right"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  </div>
+</div>
+@endsection
